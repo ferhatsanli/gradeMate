9496657c32416021dac4e72c281127fc7533f368
Ferhat Sanli <ferhatsanli.nl@gmail.com>
2025-04-12 01:17:54 +0200

    Working on: Splash screen

--- app/src/main/java/com/ferhat/grademate/MainActivity.kt
+++ app/src/main/java/com/ferhat/grademate/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
