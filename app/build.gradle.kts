a69ebbf6349e5c5d1339ee685555110bafca96b0
Ferhat Sanli <ferhatsanli.nl@gmail.com>
2025-04-11 21:14:13 +0200

    Cosmetic changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ferhat.grademate"
-        minSdk = 34
+        minSdk = 33
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
